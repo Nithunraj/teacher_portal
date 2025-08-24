0f1d0d8dfd3b7bb69dea8b94f35e0760f48978a9
Nithunraj NM <nithunraj.nm@finsurge.tech>
2025-08-24 22:22:20 +0530

    git_ignore

--- accounts/models.py
+++ accounts/models.py
@@ -55,7 +55,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     def __str__(self):
-        return self.email
+        return self.username
     
     def has_perm(self, perm, obj=None):
         return self.is_admin
